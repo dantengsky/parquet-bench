ac40e152a7f08098ebe0820a9e3e1693c2973664
dantengsky <dantengsky@gmail.com>
2022-12-28 16:39:44 +0800

    fix compile err

--- src/opendal.rs
+++ src/opendal.rs
@@ -1,12 +1,9 @@
 use anyhow::Result;
-use async_compat::Compat;
-use futures_util::stream::stream::StreamExt;
 use opendal::services::s3::Builder;
 use opendal::Operator;
-use parquet2::read::{get_page_stream, read_metadata};
-use parquet2::statistics::BinaryStatistics;
-use std::io::Cursor;
+use parquet2::read::{get_page_stream, read_metadata_async};
 use std::sync::Arc;
+use futures::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -22,16 +19,15 @@ async fn main() -> Result<()> {
     let length = meta.content_length();
     println!("blob size: {} mb", length / 1024 / 1024);
 
-    let data = object.range_read(0..length).await?;
-    let mut reader = Compat::new(data);
-    let metadata = read_metadata(&mut reader)?;
+    let mut reader = object.seekable_reader(0..);
+    let metadata = read_metadata_async(&mut reader).await?;
 
     println!("number of rows: {}", metadata.num_rows);
 
     let row_group = 0;
     let column = 0;
     let column_metadata = &metadata.row_groups[row_group].columns()[column];
-    let mut pages = get_page_stream(
+    let pages = get_page_stream(
         column_metadata,
         &mut reader,
         vec![],
@@ -39,6 +35,9 @@ async fn main() -> Result<()> {
         usize::MAX,
     )
     .await?;
+
+    //pin_mut!(pages);
+    let mut pages = Box::pin(pages);
     while let Some(_page) = pages.next().await {
         println!("got a page");
     }
