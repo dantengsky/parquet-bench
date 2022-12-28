1da776e5a3441355db7fca9652dd19207e671240
BohuTANG <overred.shuttler@gmail.com>
2022-12-28 15:45:45 +0800

    add opendal

--- src/opendal.rs
+++ src/opendal.rs
@@ -1,6 +1,12 @@
 use anyhow::Result;
+use async_compat::Compat;
+use futures_util::stream::stream::StreamExt;
 use opendal::services::s3::Builder;
 use opendal::Operator;
+use parquet2::read::{get_page_stream, read_metadata};
+use parquet2::statistics::BinaryStatistics;
+use std::io::Cursor;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,5 +21,27 @@ async fn main() -> Result<()> {
     let meta = object.metadata().await?;
     let length = meta.content_length();
     println!("blob size: {} mb", length / 1024 / 1024);
+
+    let data = object.range_read(0..length).await?;
+    let mut reader = Compat::new(data);
+    let metadata = read_metadata(&mut reader)?;
+
+    println!("number of rows: {}", metadata.num_rows);
+
+    let row_group = 0;
+    let column = 0;
+    let column_metadata = &metadata.row_groups[row_group].columns()[column];
+    let mut pages = get_page_stream(
+        column_metadata,
+        &mut reader,
+        vec![],
+        Arc::new(|_, _| true),
+        usize::MAX,
+    )
+    .await?;
+    while let Some(_page) = pages.next().await {
+        println!("got a page");
+    }
+
     Ok(())
 }
